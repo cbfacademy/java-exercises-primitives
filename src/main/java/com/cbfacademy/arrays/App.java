64c8a9a2028dba186af579c9ad38bcd83730f30d
Genyus <genyus@gmail.com>
2025-06-06 01:40:20 -0400

    feat(solutions): implement arrays task

--- src/main/java/com/cbfacademy/arrays/App.java
+++ src/main/java/com/cbfacademy/arrays/App.java
@@ -2,27 +2,56 @@ package com.cbfacademy.arrays;
 
 public class App {
     public static Integer[] createIntegerArray() {
-        // TODO: Create and return an array of 8 Integer elements
-        throw new RuntimeException("Not implemented");
+        Integer[] arr = new Integer[8];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        return arr;
     }
 
     public static Float[] createFloatArray() {
-        // TODO: Create and return an array of 12 Float elements
-        throw new RuntimeException("Not implemented");
+        Float[] arr = new Float[12];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) i;
+        }
+        return arr;
     }
 
     public static Double[] createDoubleArray() {
-        // TODO: Create and return an array of 5 Double elements
-        throw new RuntimeException("Not implemented");
+        Double[] arr = new Double[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (double) i;
+        }
+        return arr;
     }
 
     public static Boolean[] createBooleanArray() {
-        // TODO: Create and return an array of 6 Boolean elements
-        throw new RuntimeException("Not implemented");
+        Boolean[] arr = new Boolean[6];
+        boolean[] pattern = { true, false, true, false, true, false };
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = pattern[i];
+        }
+        return arr;
     }
 
     public static void printFifthElements() {
-        // TODO: Print the 5th element of each array to the screen
-        throw new RuntimeException("Not implemented");
+        Integer[] intArr = createIntegerArray();
+        Float[] floatArr = createFloatArray();
+        Double[] doubleArr = createDoubleArray();
+        Boolean[] boolArr = createBooleanArray();
+        System.out.println(intArr[4]);
+        System.out.println(floatArr[4]);
+        System.out.println(doubleArr[4]);
+        System.out.println(boolArr[4]);
     }
-} 
+
+    // Stretch task (no unit tests provided)
+    public static Object[][] createMatrix() {
+        Object[][] matrix = new Object[4][];
+        matrix[0] = createIntegerArray();
+        matrix[1] = createFloatArray();
+        matrix[2] = createDoubleArray();
+        matrix[3] = createBooleanArray();
+        return matrix;
+    }
+}
